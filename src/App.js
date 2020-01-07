27b9f124098cc3bb8ff99497a365ef5baa701be3
Sologubov Maksim <sologubovmaksim2016@gmail.com>
2020-01-07 02:07:29 +0200

    Первый commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+ 
+export default class PersonList extends React.Component {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  state = {
+    people: []
+  }
+  componentDidMount() {
+    axios.get(`http://yalantis-react-school.herokuapp.com/api/task0/users`)
+      .then(res => {
+        const people = res.data.map( p => [ p.firstName, p.lastName, p.dob, p.id]); 
+        this.setState({people});
+      })
+  }
+ 
+  render() {
+    
+    return (   
+     
+      <ol>       
+      
+        { this.state.people.map(person => 
+        <li key={person[3]}>
+          {"firstname:    " + person[0] 
+          +"  lastname:      " + person[1] 
+          + "      date of bird:    " + person[2]}          
+        </li>)}        
+      </ol>
+    )
+  }
 }
 
-export default App;
+
+
+
+
+
+// import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
